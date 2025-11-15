c7c725797c40b93a40f708b7dbb84700ed3c6c7a
Antwane Mason <git@aimai.simplelogin.com>
2025-11-15 18:31:29 -0500

    Fixed logic to exit repl loop on exit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::ExitCode;
 use std::result::Result;
 
 fn print_prompt() -> io::Result<()> {
@@ -14,23 +15,37 @@ fn read_input(buffer: &mut String) -> io::Result<()> {
     Ok(())
 }
 
-fn eval_input(buffer: &mut String) -> Result<(), Box<dyn Error>> {
+fn eval_input(buffer: &mut String) -> Result<Option<u8>, Box<dyn Error>> {
     let cmd: Vec<_> = buffer.trim().splitn(2, ' ').collect();
     match &cmd[..] {
-        ["exit", n] if n.parse::<u64>().is_ok() => Err(Box::<dyn Error>::from("")),
+        ["exit", n] => match n.parse::<u8>() {
+            Ok(exit_code) => Ok(Some(exit_code)),
+            Err(_) => Ok(None),
+        },
         _ => {
             println!("{}: command not found", buffer.trim());
-            Ok(())
+            Ok(None)
         }
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn repl() -> Result<u8, Box<dyn Error>> {
     let mut buffer = String::new();
     loop {
         print_prompt()?;
         read_input(&mut buffer)?;
-        eval_input(&mut buffer)?;
+        let result = eval_input(&mut buffer)?;
+        if let Some(exit_code) = result {
+            return Ok(exit_code);
+        }
+
         buffer.clear();
+    }
+}
+
+fn main() -> ExitCode {
+    match repl() {
+        Ok(exit_code) => ExitCode::from(exit_code),
+        Err(_) => ExitCode::FAILURE,
     }
 }

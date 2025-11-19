ea2688b93991e4c657fb969d987d4e729c1ca856
Antwane Mason <git@aimai.simplelogin.com>
2025-11-19 09:20:35 -0500

    Refactored code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::error::Error;
 #[allow(unused_imports)]
 use std::io::{self, Write};
@@ -6,6 +7,9 @@ use std::result::Result;
 
 fn print_prompt() -> io::Result<()> {
     print!("$ ");
+    /* stdout usually line-buffered and print does not produce a new line
+     * so we need to flush just in case
+     */
     io::stdout().flush()?;
     Ok(())
 }
@@ -15,28 +19,36 @@ fn read_input(buffer: &mut String) -> io::Result<()> {
     Ok(())
 }
 
-const BUILTINS: [&'static str; 3] = ["echo", "exit", "type"];
+fn builtin_echo(text: &str) -> Result<Option<u8>, Box<dyn Error>> {
+    println!("{}", text);
+    Ok(None)
+}
 
-fn eval_input(buffer: &mut String) -> Result<Option<u8>, Box<dyn Error>> {
+fn builtin_exit(arg: &str) -> Result<Option<u8>, Box<dyn Error>> {
+    match arg.parse::<u8>() {
+        Ok(exit_code) => Ok(Some(exit_code)),
+        Err(_) => Ok(None),
+    }
+}
+
+fn builtin_type(arg: &str) -> Result<Option<u8>, Box<dyn Error>> {
+    let builtins = ["echo", "exit", "type"];
+    if builtins.contains(&arg) {
+        println!("{} is a shell builtin", arg);
+        Ok(None)
+    } else {
+        // TODO use env::var(PATH) to check for executables
+        println!("{}: not found", arg);
+        Ok(None)
+    }
+}
+
+fn eval_input(buffer: &str) -> Result<Option<u8>, Box<dyn Error>> {
     let cmd: Vec<_> = buffer.trim().splitn(2, ' ').collect();
     match &cmd[..] {
-        ["echo", args] => {
-            println!("{}", args);
-            Ok(None)
-        }
-        ["exit", n] => match n.parse::<u8>() {
-            Ok(exit_code) => Ok(Some(exit_code)),
-            Err(_) => Ok(None),
-        },
-        ["type", arg] => {
-            if BUILTINS.contains(&arg) {
-                println!("{} is a shell builtin", arg);
-                Ok(None)
-            } else {
-                println!("{}: not found", arg);
-                Ok(None)
-            }
-        }
+        ["echo", args] => builtin_echo(&args),
+        ["exit", arg] => builtin_exit(&arg),
+        ["type", arg] => builtin_type(&arg),
         _ => {
             println!("{}: command not found", buffer.trim());
             Ok(None)
@@ -49,10 +61,11 @@ fn repl() -> Result<u8, Box<dyn Error>> {
     loop {
         print_prompt()?;
         read_input(&mut buffer)?;
-        let result = eval_input(&mut buffer)?;
+        let result = eval_input(&buffer)?;
         if let Some(exit_code) = result {
             return Ok(exit_code);
         }
+        // Necessary because io::stdin().read_line(buffer) appends to buffer
         buffer.clear();
     }
 }
